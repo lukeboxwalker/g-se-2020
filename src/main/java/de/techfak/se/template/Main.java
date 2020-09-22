e9146b2345ee3ff9a36443cc10f391185b062982
Yannick da Silva Bröker <ybroeker@techfak.uni-bielefeld.de>
2020-09-22 15:21:50 +0200

    Initial project

---
+++ src/main/java/de/techfak/se/template/Main.java
@@ -0,0 +1,15 @@
+package de.techfak.se.template;
+
+/**
+ * The main class, contains just the main method to start the application.
+ */
+public final class Main {
+
+    private Main() {
+    }
+
+    public static void main(final String... args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/test/java/de/techfak/se/template/MainTest.java
@@ -0,0 +1,13 @@
+package de.techfak.se.template;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void test() {
+        Assertions.assertThat(true).isTrue();
+    }
+
+}
