cd5c25ea8afa95f75e64c95e10521214267ff309
lwalkenhorst <lukas.walkenhorst@techfak.uni-bielefeld.de>
2020-12-16 18:41:08 +0100

    Remove methods in BoardImpl

--- src/main/java/de/techfak/se/lwalkenhorst/game/BoardImpl.java
+++ src/main/java/de/techfak/se/lwalkenhorst/game/BoardImpl.java
@@ -6,12 +6,6 @@ import java.util.List;
 
 public class BoardImpl implements Board {
     private static final int DEFAULT_START_COL = 7;
-    private static final int FIVE = 5;
-    private static final int THREE = 3;
-    private static final int TWO = 2;
-    private static final int ONE = 1;
-
-    private final int[] pointsPerCol;
     private final int startColumn;
 
     private final TileImpl[][] tiles;
@@ -20,10 +14,7 @@ public class BoardImpl implements Board {
     public BoardImpl(final TileImpl[][] tiles, final Bounds bounds) {
         this.tiles = tiles;
         this.bounds = bounds;
-
         this.startColumn = DEFAULT_START_COL;
-        this.pointsPerCol = new int[]{FIVE, THREE, THREE, THREE, TWO, TWO, TWO, ONE,
-            TWO, TWO, TWO, THREE, THREE, THREE, FIVE};
     }
 
     @Override
@@ -46,42 +37,19 @@ public class BoardImpl implements Board {
         return startColumn;
     }
 
-
     @Override
-    public String toString() {
-        return "AbstractBoard{" +
-            "tiles=" + Arrays.deepToString(tiles) +
-            '}';
+    public Iterator<Tile> iterator() {
+        return Arrays.stream((Tile[][]) tiles).flatMap(Arrays::stream).iterator();
     }
 
     public void cross(final List<CellPosition> cellPositions) {
         cellPositions.forEach(position -> getTileAt(position).cross());
     }
 
-    public boolean isColumnFull(final int col) {
-        if (new CellPosition(col, 0).isInside(this.getBounds())) {
-            boolean isFull = true;
-            for (int row = 0; row < getBounds().getRows(); row++) {
-                if (!getTileAt(new CellPosition(col, row)).isCrossed()) {
-                    isFull = false;
-                    break;
-                }
-            }
-            return isFull;
-        }
-        return false;
-    }
-
-    public int getPointsForCol(final int col) {
-        if (new CellPosition(col, 0).isInside(this.getBounds())) {
-            return pointsPerCol[col];
-        } else {
-            return 0;
-        }
-    }
-
     @Override
-    public Iterator<Tile> iterator() {
-        return Arrays.stream((Tile[][]) tiles).flatMap(Arrays::stream).iterator();
+    public String toString() {
+        return "AbstractBoard{" +
+            "tiles=" + Arrays.deepToString(tiles) +
+            '}';
     }
 }
